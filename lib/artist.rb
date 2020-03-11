28bbbd95abffb1893201b8fe2418bf41df17146c
William Lin <willlin1996@gmail.com>
2020-03-11 16:10:12 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+class Artist 
+  attr_accessor :name
+  @@all = []
+  def initialize(name) 
+    @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def songs(song)
+  end
+end
+
+class Song 
+end
