a863c950dc56f36fb49e66b029cf3070968a90b6
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-03-11 10:58:20 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist
+    @@all = []
+    attr_accessor :name 
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+    def print_songs
+        songs.each {|song| puts song.name}
+    end
+
+    def self.find_or_create_by_name(name)
+        artist = @@all.find {|artist| artist.name == name}
+        artist || self.new(name)
+    end
+    def self.all 
+        @@all 
+    end
+end
---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song 
+
+    @@all = []
+    attr_accessor :name, :artist
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all 
+        return @@all 
+    end
+    def self.new_by_filename(fn)
+        puts fn
+        pair = fn.split(" - ")
+        artist = pair[0]
+        song = pair[1]
+        s = self.new(song)
+        a = Artist.new(artist)
+        a.add_song(s)
+        #TODO:
+    end
+end
